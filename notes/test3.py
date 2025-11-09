98dc40e66e08630c1e2a537d96f1c2bee90ec3fb
KevinX <Xzb04720638k>
2025-11-09 17:07:02 -0500

    update

---
+++ notes/test3.py
@@ -0,0 +1,66 @@
+set1 = {0, 1, True, False, None, 2.3, 2.3, 'abc', 'abc', (11, 22, 33), (11, 22, 33)}
+print(type(set1), set1)
+
+set2 = set()
+print(type(set2), set2)
+
+set3 = frozenset([0, 1, True, False, None, 2.3, 2.3, 'abc', 'abc', (11, 22, 33), (11, 22, 33)])
+set4 = frozenset({0, 1, True, False, None, 2.3, 2.3, 'abc', 'abc', (11, 22, 33), (11, 22, 33)})
+print(type(set3), set3)
+print(type(set4), set4)
+
+set5 = frozenset()
+print(type(set5), set5)
+
+set1.add(12.34)
+print(set1)
+set1.update([31, 32])
+print(set1)
+set1.update({33})
+print(set1)
+set1.update((34, ))
+print(set1)
+set1.remove(34)
+print(set1)
+set1.discard(33)
+print(set1)
+print(set1.pop())
+print(set1.pop())
+set1.clear()
+print(set1)
+
+s1 = {10, 20, 30, 40, 50}
+s1.remove(20)
+s1.add(60)
+print(s1)
+
+print(10 in s1)
+print(110 not in s1)
+
+s1 = {10, 20, 30, 40, 50}
+s2 = {30, 40, 50, 60, 70}
+print(s1.difference(s2))
+s1.difference_update(s2)
+print(s1)
+
+print(s1.union(s2))
+
+s3 = {30, 40, 50}
+print(s3.issubset(s2))
+print(s2.issuperset(s3))
+print(s2.isdisjoint(s1))
+
+s1 = {10, 20, 30, 40, 50, 60}
+s2 = {40, 50, 60, 70, 80, 90}
+print( s1 | s2)
+print(s1 & s2)
+print(s1 - s2)
+print(s1 ^ s2)
+
+for item in s1:
+    print(item, end=' ')
+print()
+
+for item in frozenset(s1):
+    print(item, end=' ')
+print()
