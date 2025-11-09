46347cef310679ab5d2ea61df188ec783a21e6c7
KevinX <Xzb04720638k>
2025-11-09 16:46:44 -0500

    update

--- notes/test2.py
+++ notes/test2.py
@@ -60,3 +60,96 @@ print()
 for index, element in enumerate(score_list, start=1):
     print(index, element, end='\t')
 print()
+
+tuple1 = (1, True, False, None, 2.2, 'abc', (11, 22, 33))
+print(type(tuple1), tuple1, tuple1[1])
+
+tuple2 = ()
+tuple3 = tuple()
+print(tuple2, tuple3)
+
+tuple5 = ('a',)
+print(type(tuple5), tuple5)
+
+tuple6 = (1, True, False, None, 2.2, 'abc', [11, 22, 33])
+tuple6[6][1] = 222
+print(tuple6)
+print(tuple6.index(2.2))
+print(tuple6.count(2.2))
+
+t1 = (23, 11, 32, 30, 17)
+print(len(t1))
+print(max(t1))
+print(min(t1))
+print(sum(t1))
+t2 = sorted(t1, reverse=True)
+print(t2)
+
+index = 0
+while index < len(t2):
+    print(t2[index], end='\t')
+    index += 1
+print()
+
+for i in t2:
+    print(i, end='\t')
+print()
+
+def test(*args):
+    print(f'我是test函数，我收到的参数是：{args}，参数类型是：{type(args)}')
+list1 = [100, 200, 300, 400, 300]
+tuple1 = ('你好', '北京', '尚硅谷', '北京')
+test(list1)
+test(tuple1)
+test(*list1)
+test(*tuple1)
+
+msg = 'welcome@to@atguigu'
+print(msg[1])
+print(msg[-1])
+print(msg.index('l'))
+print(msg.split('@'))
+print(msg.replace('@', '%'))
+print(msg.count('@'))
+
+msg = '34215尚12硅34谷4132'
+print(msg.strip('2345'))
+msg = ' 尚硅谷 '
+print(msg.strip(' '))
+
+print(len(msg))
+
+msg = 'welcome to atguigu'
+index = 0
+while index < len(msg):
+    print(msg[index], end=' ')
+    index += 1
+print()
+
+for item in msg:
+    print(item, end=' ')
+print()
+
+list1 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+print(list1[::])
+print(list1[0:8:2])
+print(list1[8:0:-2])
+print(list1[::-1])
+tuple1 = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+print(tuple1[0:5:2])
+print(msg[0:5:2])
+
+list1 = [10, 20, 30, 40]
+list2 = [50, 60, 70, 80]
+print(list1 + list2)
+tuple1 = (10, 20, 30, 40)
+tuple2 = (50, 60, 70, 80)
+print(tuple1 + tuple2)
+str1 = 'hello'
+str2 = 'atguigu'
+print(str1 + str2)
+
+print(list1 * 2)
+print(tuple1 * 2)
+print(str1 * 2)
+
