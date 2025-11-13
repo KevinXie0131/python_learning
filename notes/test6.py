842dd069cfab02239143fb0064c38c0a5f79c307
KevinX <Xzb04720638k>
2025-11-12 22:23:27 -0500

    update

---
+++ notes/test6.py
@@ -0,0 +1,19 @@
+var1: int = 12
+var5 = 12 # type: int
+var2: float = 3.45
+var3: str = 'abc'
+var4: bool = True
+
+class Student:
+    pass
+
+stu: Student = Student()
+
+my_list1: list[int] = [1, 2, 3]
+my_list2 = [1, 2, 3] # type: list[int]
+my_tuple1: tuple[int, float, str] = (1, 1.2, 'str')
+
+def my_fun():
+    return 10
+
+result = my_fun() # type: int
