2925e60d68192dec3948c97ec60a514d4481a4e4
KevinX <Xzb04720638k>
2025-11-14 15:20:03 -0500

    update

--- notes/test6.py
+++ notes/test6.py
@@ -8,6 +8,10 @@ class Student:
     pass
 
 stu: Student = Student()
+stu1: Student = Student()
+print('stu is stu1: ', stu is stu1)
+print('stu is Student: ', isinstance(stu, Student))
+print('stu is Student: ', type(stu) == Student)
 
 my_list1: list[int] = [1, 2, 3]
 my_list2 = [1, 2, 3] # type: list[int]
@@ -28,3 +32,60 @@ def func1(data: list[int]) -> int:
     return len(data)
 
 print(func1([1, 2]))
+
+
+# 导入json模块
+import json
+
+json_data_string = '''
+{
+    "name": "Alice",
+    "age": 30,
+    "isStudent": false,
+    "courses": [
+        {"title": "History 101", "credits": 3},
+        {"title": "Math 202", "credits": 4}
+    ],
+    "address": null
+}
+'''
+
+# 通过 json.loads(data) 方法把json数据转化为了 python数据
+data: dict = json.loads(json_data_string)
+print(data)
+print(type(data))
+print(data.keys())
+print(data.get('courses'))
+
+# 通过 json.dumps(data) 方法把python数据转化为了 json数据
+data = json.dumps(data)
+print(data)
+print(type(data))
+
+
+def account_create(initial_amount=0):
+
+    def atm(num, deposit=True):
+        nonlocal initial_amount
+        if deposit:
+            initial_amount += num
+            print(f"存款：+{num}， 账户余额：{initial_amount}")
+        else:
+            initial_amount -= num
+            print(f"取款：-{num}， 账户余额：{initial_amount}")
+
+    return atm
+
+atm = account_create()
+
+atm(100)
+atm(200)
+atm(100, deposit=False)
+
+
+positive_infinity  = float('-inf')
+print(positive_infinity )
+print(type(positive_infinity))
+
+print(max(1, 2))
+print(min(1, 2))
